38d1081954a7b0776d77178ba35ae7b3ef18fc58
Zoe <hxy9349@gmail.com>
2025-05-19 01:10:50 +0800

    Chap12_3

---
+++ Program3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Chap12Test3_3
+{
+    internal class Program
+    {
+        public static void FeetToInches(double inches)
+        {
+            double feet = inches / 12;
+            Console.WriteLine($"{inches} 英吋 = {feet} 英尺");
+        }
+        public static void YardsToInches(double inches)
+        {
+            double yards = inches / 36;
+            Console.WriteLine($"{inches} 英吋 = {yards} 英碼");
+        }
+
+        public delegate void ConvertDelegate(double inches);
+
+        static void Main(string[] args)
+        {
+            Console.Write("請輸入英吋：");
+            double inches = double.Parse(Console.ReadLine());
+
+            ConvertDelegate[] converters = {
+                FeetToInches,
+                YardsToInches
+            };
+
+            foreach (var converter in converters)
+            {
+                converter(inches);
+            }
+        }
+    }
+}
