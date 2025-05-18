20b57b3a2e4989126de719f11081b310c32f541f
Zoe <hxy9349@gmail.com>
2025-05-19 01:11:50 +0800

    Chap13_3

---
+++ Form1.cs
@@ -0,0 +1,16 @@
+namespace Chap13Test3_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.MouseDown += label1_MouseDown;
+        }
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            label1.BackColor = (e.Button == MouseButtons.Left) ? Color.Red : 
+                (e.Button == MouseButtons.Right) ? Color.Green :label1.BackColor;
+        }
+    }
+}
